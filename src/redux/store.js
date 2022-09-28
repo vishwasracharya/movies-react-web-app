ee05a335b69e8aec7d3b8dc6a68996d78bf61801
Vishwas Acharya <vishwasracharya@gmail.com>
2022-09-28 18:25:45 +0530

    createStore visual warning

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,8 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
