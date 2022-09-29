ea5b58691becfdf0a0d6c81778837baf3d3d67c1
Vishwas Acharya <vishwasracharya@gmail.com>
2022-09-29 15:52:07 +0530

    added meta tags

--- src/components/Metadata/Metadata.js
+++ src/components/Metadata/Metadata.js
@@ -5,6 +5,9 @@ const Metadata = ({ title }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="description" content="A React App which uses the existing api developed with nodejs." />
+      <meta name="keywords" content="react, nodejs, api, authentication, authorization, jwt, cookies" />
+      <meta name="author" content="Vishwas Acharya" />
     </Helmet>
   );
 };
